8d9164b48a56094d8106346d8c2e162727e7d90e
meghana786 <160600002+meghana786@users.noreply.github.com>
2024-03-15 19:25:35 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+var virender_img=document.getElementById("virender-img");
+var imgClick=document.getElementById("virender");
+imgClick.addEventListener("click",function(){
+  virender_img.style.display="block";
+});
+
+var sachin_img=document.getElementById("sachin-img");
+var sachin=document.getElementById("sachin");
+sachin.addEventListener("click",function(){
+  sachin_img.style.display="block";
+});
+
+var gautam_img=document.getElementById("gautam-img");
+var gautam=document.getElementById("gautam");
+gautam.addEventListener("click",function(){
+  gautam_img.style.display="block";
+});
+
+var virat_img=document.getElementById("virat-img");
+var virat=document.getElementById("virat");
+virat.addEventListener("click",function(){
+  virat_img.style.display="block";
+});
+
+var dhoni_img=document.getElementById("dhoni-img");
+var dhoni=document.getElementById("dhoni");
+dhoni.addEventListener("click",function(){
+  dhoni_img.style.display="block";
+});
+
+
+var yuvaraj_img=document.getElementById("yuvaraj-img");
+var yuvaraj=document.getElementById("yuvaraj");
+yuvaraj.addEventListener("click",function(){
+  yuvaraj_img.style.display="block";
+});
